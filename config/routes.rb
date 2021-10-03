27de10f37f2a60dffa571a153e5656e1dbaccd7d
Lalitha Malla <53849041+Lalitha01M@users.noreply.github.com>
2021-10-03 10:41:27 +0530

    Update routes.rb
    format updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # get "users", to: "users#index"
   post "users/login", to: "users#login"
-  resources:users
-  resources:todos
+  resources :users
+  resources :todos
 
 end
