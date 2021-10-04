8110f764111f67b73664bd4a73c6b1e04720f1b8
Lalitha Malla <53849041+Lalitha01M@users.noreply.github.com>
2021-10-04 20:12:16 +0530

    Update todos_controller.rb
    changed todo_test to todo_text

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -14,7 +14,7 @@ class TodosController < ApplicationController
   end
 
   def create
-    todo_text = params[:todo_test]
+    todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
     response_text = "hey, your new todo is created with the id #{new_todo.id}"
