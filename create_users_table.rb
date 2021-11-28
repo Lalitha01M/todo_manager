5fb2e444c8ff807320e4678418cde8369b0ba2e2
Lalitha01M <mlalitha@gnits.ac.in>
2021-11-28 08:26:01 +0530

    migration done

--- create_users_table.rb
+++ create_users_table.rb
@@ -1,6 +1,11 @@
 require './connect_db.rb'
 connect_db!
 
+ActiveRecord::Migration.create_table(:todos) do |t|
+  t.column :todo_text, :text
+  t.column :due_date, :Date
+  t.column :completed, :boolean
+end
 ActiveRecord::Migration.create_table(:users) do |u|
   u.column :name, :text
   u.column :email, :text
