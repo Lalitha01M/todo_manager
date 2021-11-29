bb3e7335d9f6643240083ac53c903dbc48011c89
Lalitha01M <mlalitha@gnits.ac.in>
2021-11-29 16:03:24 +0530

    error massages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
       session[:current_user_id] = user.id
       redirect_to "/"
     else
-      render plain: "incorrect login"
+      flash[:error] = "your login attempt was invalid, please retry"
+      redirect_to new_session_path
     end
   end
 
